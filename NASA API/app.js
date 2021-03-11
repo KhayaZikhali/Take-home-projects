2bb796a59f88a216bf34f5f498abf50e6da0c9b5
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-03-11 14:35:04 +0100

    Finished the code

--- NASA API/app.js
+++ NASA API/app.js
@@ -1,7 +1,6 @@
 let button = document.querySelector(".button");
 let colors = [];
-let name = 
-//API url 
+//API url
 let url = `https://api.nasa.gov/planetary/apod?api_key=oX4gl6LkhDhmyQ7TzEAEUZamvQqRbnSg56IorvLI`;
 fetch(url)
    .then(response => {return  response.json()})
@@ -10,8 +9,11 @@ fetch(url)
         console.log(data)
         colors.push(data.url)
         })
-       
+       console.log(colors);
 button.addEventListener("click", function () {
   // set the background imge to the last item in the colors array
     document.body.style.backgroundImage = "url(" + colors[Math.random() * (colors.length-1)] + ")"
 });
+
+
+// ! This unfortunately does not run on Codepen.io
